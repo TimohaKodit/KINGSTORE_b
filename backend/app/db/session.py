5b540d982489f6c961f5e88da2d0c35da2995a92
TimohaKodit <63901163+TimohaKodit@users.noreply.github.com>
2025-11-05 00:01:57 +0500

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
-
+DATABASE_URL = "postgresql://${{PGUSER}}:${{POSTGRES_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:5432/${{PGDATABASE}}"
 # Настройка движка и сессии
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False} # Только для SQLite
+    DATABASE_URL,
+    # **DO NOT** include connect_args={"check_same_thread": False} here!
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
